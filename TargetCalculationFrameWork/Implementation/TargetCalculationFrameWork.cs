02bfc1f2f96c6704865f2930869790f86ea45e36
Huber, Marco <marco.huber@sivantos.com>
2024-01-11 14:48:45 +0100

    Types of FittingFormula.dlls in same folder are shown

--- TargetCalculationFrameWork/Implementation/TargetCalculationFrameWork.cs
+++ TargetCalculationFrameWork/Implementation/TargetCalculationFrameWork.cs
@@ -1,26 +1,47 @@
 ﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection;
 using TargetCalculationInterfaces;
 
 namespace TargetCalculationFrameWork
 {
     internal class TargetCalculationFrameWork : ITargetCalculationFrameWork
     {
-        private string _targetFormulaDllSubString = "TargetFormula";
+        private readonly string _targetFormulaDllSubString = "TargetFormula";
+        private readonly string _dllExtension = ".dll";
 
         public IList<TargetFormula> GetAvailableTargetFormulas()
         {
+            var targetFormulaList = new List<TargetFormula>();
+
             var currentDirectory = Directory.GetCurrentDirectory();
 
             var files = Directory.GetFiles(currentDirectory);
 
             // Filter files containing the target substring
-            var matchingFiles = files.Where(file => file.Contains(_targetFormulaDllSubString)).ToArray();
+            var targetFormulaFilePathes = files.Where(file => file.Contains(_targetFormulaDllSubString) && file.Contains(_dllExtension)).ToArray();
 
+            foreach (var targetFormulaFilePath in targetFormulaFilePathes)
+            {
+                // Load the assembly from the DLL
+                var assembly = Assembly.LoadFile(targetFormulaFilePath);
 
-            return new List<TargetFormula> { TargetFormula.NalNl2, TargetFormula.DslV5, TargetFormula.ThirdGain };
+                // Get the type (class) from the assembly
+                var targetCalculationFactoryType = assembly.GetTypes().First(type => type.ToString().Contains("TargetCalculationFactory"));
+
+                var targetCalculationFactory =
+                    Activator.CreateInstance(targetCalculationFactoryType) as ITargetCalculationFactory;
+
+                var targetFormula = targetCalculationFactory?.GetTargetFormula();
+
+                if(targetFormula.HasValue)
+                    targetFormulaList.Add(targetFormula.Value);
+            }
+
+            return targetFormulaList;
         }
     }
 }
